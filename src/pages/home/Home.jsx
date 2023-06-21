c58a8f4b961c1a1aff6ce6346020f4d3b213043d
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-06-22 01:30:54 +0500

    initial UI completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 // css
 import './home.css'
@@ -25,7 +26,7 @@ export default function Home() {
             setPrefectures(response.data.result);
         })
     }, [])
-    
+
     // for fetching population composition
     useEffect(() => {
         axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${selectedPrefecture}`, {
@@ -42,15 +43,44 @@ export default function Home() {
         setSelectedPrefecture(prefCode)
     }
 
+    const data = [
+        { year: 2010, value: 2400 },
+        { year: 2011, value: 1800 },
+        { year: 2012, value: 3600 },
+        { year: 2013, value: 2400 },
+        { year: 2014, value: 3100 },
+        { year: 2015, value: 4000 },
+        { year: 2016, value: 5000 },
+    ];
+
     return (
         <div className='home'>
-            {prefectures.map(prefecture => <CheckBox
-                key={prefecture.prefCode}
-                prefName={prefecture.prefName}
-                id={prefecture.prefCode}
-                setSelectedPrefectureHandler={setSelectedPrefectureHandler}
-                selectedPrefecture={selectedPrefecture}
-            />)}
+            <div className='homePref'>
+                {prefectures.map(prefecture => <CheckBox
+                    key={prefecture.prefCode}
+                    prefName={prefecture.prefName}
+                    id={prefecture.prefCode}
+                    setSelectedPrefectureHandler={setSelectedPrefectureHandler}
+                    selectedPrefecture={selectedPrefecture}
+                />)}
+            </div>
+
+            <select name="" id="">
+                <option value="総人口">Total Population</option>
+                <option value="年少人口">Population Under 15</option>
+                <option value="生産年齢人口">Working-Age Population</option>
+                <option value="老年人口">Elderly Population</option>
+            </select>
+
+            <LineChart width={600} height={400} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="value" stroke="#8884d8" />
+            </LineChart>
+
         </div>
     )
 }
